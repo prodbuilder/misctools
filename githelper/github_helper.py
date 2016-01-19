a1df82c507e2d9848b494df0e45f596b2361d2a1
Yu Guo <yguo@thumbtack.com>
2016-01-19 15:04:41 -0800

    WIP: githelper - need fix issue create time

--- githelper/github_helper.py
+++ githelper/github_helper.py
@@ -6,19 +6,15 @@
 # Instant
 #               -- Go abroad for further study & grow
 #
-# helper to print out recent activities of team members
-# including current milestone open issues,
-# all recent closed issues, recent commits
-# first command line parameter is user name
-# checks by prefix of user name and return first match
-# if no parameter, then print unassigned issues in current milestone
+# helper to print out recent activities of multiple github accounts
+# Similar to contribution counted by github, but exclude issues
 #
-# Assumes you save your user, ps in local file 'ps.txt'
-# use: python github_helper.py appledore
+# Assumes you save your user, ps in local file 'ps.txt', 'ps2.txt'
+# use: python github_merger.py
 #
 #--------------------------------------------------------------
 #
-# Date:     2015-11-01
+# Date:     2016-01-19
 #
 # Author:   1p3akproduct@gmail.com
 #
@@ -30,6 +26,9 @@ import github
 import getpass
 import logging
 import argparse
+import time
+from collections import Counter, OrderedDict
+from datetime import date, timedelta, datetime
 
 __version__ = "0.0.1"
 __version_info__ = __version__.split('.')
@@ -37,14 +36,17 @@ __version_info__ = __version__.split('.')
 #--------------------------------------------------------------
 # Global Constants & Vars
 #--------------------------------------------------------------
-
-REPO = 'Chipmunk'
-NUM_ISSUES = 50
-
+DEBUG = False
+START = datetime(2015, 8, 30)
 #--------------------------------------------------------------
 # Global Functions
 #--------------------------------------------------------------
 _local = tzlocal.get_localzone()
+
+def _e(val):
+    if isinstance(val, unicode):
+        return val.encode('utf8')
+    return val
 
 def _local_time(tt):
     """ github created_at time was naive
@@ -53,20 +55,107 @@ def _local_time(tt):
     aware = pytz.utc.localize(tt)
     return aware.astimezone(_local).strftime('%Y-%m-%d %I:%M %p')
 
+def _d2s(date_obj, format = '%Y-%m-%d'):
+    """ date object to time"""
+    aware = pytz.utc.localize(date_obj)
+    return aware.astimezone(_local).strftime(format)
+
+def _s2d(date_str, format = '%Y-%m-%d'):
+    """ from string to date object"""
+    return datetime.strptime(date_str, format)
+
 def _log(msg, msg_type='info'):
     print ('[%s] %s' % (msg_type, msg)).encode('utf8')
+
+def print_dict(d):
+    for k,v in d.iter():
+        print '%-15s: %s' %k, v
+
+# Helper functions
+
+def commit_header(string):
+    """first line of commit msg"""
+    return string.split('\n')[0] if string else ''
+
+def commit_info(commit):
+    """ date, author and header of commit msg"""
+    if type(commit) == github.Commit.Commit:
+        return _d2s(commit.commit.author.date), \
+                commit.author.login, \
+                commit_header(commit.commit.message), \
+                'commit'
+
+def issue_info(issue):
+    """ date, creater and title of issue"""
+    if type(issue) == github.Issue.Issue:
+        return _d2s(issue.created_at), \
+               issue.user.login, \
+               issue.title, \
+               'issue'
+
+def filter_date(time_obj, year=None, month=None, day = None):
+    """filter by year, month date of time_obj"""
+    m_year = year is None or time_obj.tm_year == year
+    m_month = month is None or time_obj.tm_mon == month
+    m_day = day is None or time_obj.tm_mday == day
+    return m_year and m_month and m_day
+
+def filter_msg(msg):
+    """only allow commits with certain msg signature"""
+    merge = msg.startswith("Merge branch")
+    readme = 'readme' in msg.lower()
+    # return (not merge) and (not readme)
+    return not merge
+
+
+def counts_to_clog(counts):
+    """
+    take a Count() dict: {date_str: count}
+    turn into contribution log of to feed into streak
+    """
+    dates = counts.keys()
+    start, end = max(START, _s2d(min(dates))), _s2d(max(dates))
+    datei = start
+    c_log = OrderedDict()
+    while datei <= end:
+        if DEBUG: print '%s: %s contributions'% (_d2s(datei), counts.get(_d2s(datei), 0))
+        c_log[_d2s(datei)] = counts.get(_d2s(datei), 0)
+        datei += timedelta(days=1)
+    return c_log
+
+def streak(contribution_lst):
+    """
+    find current and longest streak
+    @contribution_lst: [0, 0, 13, 0, 3, 2, 0,1,2,1,1,0,0,10,1,2]
+    """
+    curr_streak, longest_streak = 0, 0
+    for n in contribution_lst:
+        if n == 0:
+            curr_streak = 0
+        else:
+            curr_streak += 1
+            if curr_streak > longest_streak:
+                longest_streak = curr_streak
+        if DEBUG: print 'curr = %s, curr_streak = %s, longest_streak = %s' %(n, curr_streak, longest_streak)
+    return curr_streak, longest_streak
+
 
 #--------------------------------------------------------------
 # Classes
 #--------------------------------------------------------------
-class GitLogin(object):
+class GitHelper(object):
     """login with user,ps in ps.txt
     if doesn't exist, prompt user to input
     save to ps.txt optionally
     """
-    def __init__(self):
-        self.FILENAME = 'ps.txt'
+    def __init__(self, filename):
+        self.FILENAME = filename
         self.tryLogin()
+        self.contributions = self.load_commits() + self.load_issues()
+        if DEBUG: _log('%s contributions loaded for %s' % (len(self.contributions), self.USER), 'Info')
+        self.by_day = Counter()
+        self.daily_log = OrderedDict()
+
 
     def loadFromFile(self):
         _log('Trying to load USER, PASSWORD from %s...' %self.FILENAME, 'Info')
@@ -91,7 +180,7 @@ class GitLogin(object):
         while True:
             try:
                 self.g = github.Github(self.USER, self.PASSWORD)
-                _log('Login Success!', 'Info')
+                _log('Login as %s: Success!' %self.USER, 'Info')
                 if wronginfo:
                     self.saveUserPass()
                 break
@@ -100,134 +189,78 @@ class GitLogin(object):
                 self.livePrompt()
                 wronginfo = True
 
+    def load_commits(self):
+        return [tuple([repo.name]+list(commit_info(commit)))
+              for repo in self.g.get_user().get_repos()
+              if repo.size > 0
+              for commit in repo.get_commits(author=self.USER)
+              if filter_msg(_e(commit_header(commit.commit.message)))]
 
-class GitHelper(GitLogin):
+    def load_issues(self):
+        return [tuple([repo.name]+list(issue_info(issue)))
+              for repo in self.g.get_user().get_repos()
+              if repo.size > 0
+              for issue in repo.get_issues()
+              if issue.user.login == self.USER]
 
-    def __init__(self):
-        GitLogin.__init__(self)
-        self.repo = [repo for repo in self.g.get_user().get_repos() if repo.name == REPO].pop()
-        self.team = self.repo.get_assignees()
-        self.issues = self.repo.get_issues()
-        self.closed_issues = self.repo.get_issues(state='closed')
-        self.milestones = self.repo.get_milestones()
+    def count_contributions_by_day(self):
+        for repo, date_str, author, title, ctype in self.contributions:
+            self.by_day[date_str] += 1
 
-    def getTeam(self):
-        print '\nTeam members are:\n'
-        for each in self.team:
-            print '     %-18s %-18s' %(each.login or '', each.name or '')
+    def counts_to_clog(self):
+        self.daily_log = counts_to_clog(self.by_day)
 
-    def getMember(self, input_user):
-        if not input_user:
-            return
-        check_user = str(input_user).lower()
-        for each in self.team:
-            if str(each.login).lower().startswith(check_user) or str(each.name).lower().startswith(check_user):
-                return each
+    def longest_streak(self):
+        self.curr_streak, self.longest_streak = streak(self.daily_log.values())
 
+    def show(self):
+        if DEBUG:
+            for c in self.contributions:
+                print c
+        print '--------------- %s: total %s contributions ------------' % (self.USER, len(self.contributions))
+        print '--------------- count by day ----------------'
+        print self.by_day
+        print '--------------- counts to contribution log --------'
+        print self.daily_log
+        print 'current streak = %s, longest streak = %s' %(self.curr_streak, self.longest_streak)
 
-    def formatIssue(self, issue):
-        return '    %s#%-3s %-50s   %s' %(
-            '%s  By: %-18s ' % (_local_time(issue.closed_at), issue.closed_by.login) if issue.closed_at else '',
-            issue.number,
-            issue.title,
-            ', '.join(['<%s>' % l.name for l in issue.labels]))
+    def tabulate(self):
+        self.count_contributions_by_day()
+        self.counts_to_clog()
+        self.longest_streak()
+        # self.show()
 
+class GitMerger(GitHelper):
 
-    def getUserOpenIssues(self, user):
-        formatted_issues = [self.formatIssue(issue)
-            for issue in self.repo.get_issues(
-                milestone = self.milestone,
-                assignee=user,
-                state='open')]
-        if formatted_issues:
-            print '\n## Open Issues: %s' % len(formatted_issues)
-            for i in formatted_issues:
-                print i
-        else:
-            print '\n## No Open Issues.'
+    def __init__(self, filenames):
+        self.FILENAME = filenames
+        self.contributions = []
+        self.USER = []
+        self.by_day = Counter()
+        self.daily_log = OrderedDict()
 
-
-    def getUserClosedIssues(self, user):
-        print '\n## Recently Closed Issues:'
-        formatted_issues = [self.formatIssue(issue)
-            for issue in self.closed_issues[:NUM_ISSUES]
-            if issue.assignee
-            if issue.assignee.login.lower() == user.login.lower()
-            if issue.state == 'closed']
-        for i in formatted_issues:
-            print i
-
-
-    def getUserRecentCommits(self, n=15, **kargs):
-        print '\n## Recent commits:'
-        user_commits = self.repo.get_commits(**kargs)
-        for commit in user_commits[:n]:
-            try:
-                print '    %s  By: %-18s %s' %(
-                    _local_time(commit.get_statuses()[0].created_at),
-                    commit.author.login,
-                    commit.commit.message.split('\n').pop(0)[:80])
-            except:
-                print '    sth wrong~~~'
+    def merge(self):
+        for f in self.FILENAME:
+            acc = GitHelper(f)
+            self.USER.append(acc.USER)
+            self.contributions += acc.contributions
 
 
-    def listMilestones(self):
-        for m in self.milestones:
-            print '       %-20s [Due on: %s] Open: %-3s Closed: %-s' % \
-                (m.title,
-                 _local_time(m.due_on),
-                 m.open_issues,
-                 m.closed_issues)
-
-
-    def getOverview(self, input_user=None, i_milestone=0):
-        user = self.getMember(input_user)
-        self.milestone = self.milestones[i_milestone]
-
-        self.listMilestones()
-
-        if user:
-            print '\n%s -> %s, %s @ %s [Due on: %s]' % (
-                input_user,
-                user.login,
-                user.name or '',
-                self.milestones[i_milestone].title,
-                _local_time(self.milestone.due_on))
-            self.getUserOpenIssues(user)
-            self.getUserClosedIssues(user)
-            self.getUserRecentCommits(author=user)
-        else:
-            self.getTeam()
-            print '\n==========================\nUnassigned @ %s [Due on: %s]' % (self.milestone.title, _local_time(self.milestone.due_on))
-            self.getUserOpenIssues(github.GithubObject.NotSet)
-
-            for member in self.team:
-                print '\n==========================\n>>>>>> %s, %s' % (member.login or '', member.name or '')
-                self.getUserOpenIssues(member)
-
-            self.getUserRecentCommits(n=30)
 
 
 #--------------------------------------------------------------
 # Entry
 #--------------------------------------------------------------
 
-def get_arg():
-    """ arg parser """
-    parser = argparse.ArgumentParser(
-        description="quick list of who's working on what Github Chipmunk.")
-    parser.add_argument('-u', '--user', default=None, help="allows prefix match")
-    parser.add_argument('-m', '--milestone', type=int, default=0, help="0-current milestone, 1-next milestone, etc. Larger number than available milestone is ignored.")
-
-    args = parser.parse_args()
-    print args.__dict__
-    return args
-
 def main():
-    args = get_arg()
-    githelper = GitHelper()
-    githelper.getOverview(args.user, args.milestone)
-
+    accounts = ['ps1.txt', 'ps2.txt']
+    # g=GitHelper(accounts[0])
+    # g.tabulate()
+    # g.go()
+    merger = GitMerger(accounts)
+    merger.merge()
+    merger.tabulate()
+    merger.show()
 
 if __name__ == '__main__':
     main()
